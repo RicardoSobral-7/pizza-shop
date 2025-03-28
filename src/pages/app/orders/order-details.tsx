c47ca2f34b99a6287a459b376471102d19c8a4de
Ricardo Sobral <riki.sobral@gmail.com>
2025-03-27 21:42:22 -0300

    feature: status change

--- src/pages/app/orders/order-details.tsx
+++ src/pages/app/orders/order-details.tsx
@@ -1,98 +1,123 @@
+import { getOrderDetails } from "@/api/get-order-details";
+import { OrderStatus } from "@/components/order-status";
 import { DialogContent, DialogDescription, DialogHeader, DialogTitle } from "@/components/ui/dialog";
 import { Table, TableBody, TableCell, TableFooter, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { useQuery } from "@tanstack/react-query";
+import { formatDistanceToNow } from "date-fns";
+import { ptBR } from "date-fns/locale";
 
+interface OrderDetailsProps {
+  orderId: string
+  open: boolean
+}
 
-export function OrderDetails() {
+export function OrderDetails({ orderId, open }: OrderDetailsProps) {
+
+  // o open vem para habilitar a request
+  const { data: orderDetails } = useQuery({
+    queryKey: ['order', orderId],
+    queryFn: () => getOrderDetails({ orderId }),
+    enabled: open
+  })
+
   return (
     <DialogContent>
       <DialogHeader>
         <DialogTitle>
-          Pedido: fsdjfjsi123435rASA
+          Pedido: {orderId}
         </DialogTitle>
         <DialogDescription>
           Detalhes do pedido
         </DialogDescription>
       </DialogHeader>
-      <div className="space-y-6">
-        <Table>
-          <TableBody>
-            <TableRow>
-              <TableCell className="text-muted-foreground">
-                Status
-              </TableCell>
-              <TableCell className="flex justify-end">
-                <div className="flex items-center gap-2">
-                  <span className="h-2 w-2 rounded-full bg-slate-400" />
-                  <span className="font-medium text-muted-foreground">Pendente</span>
-                </div>
-              </TableCell>
-            </TableRow>
+      {orderDetails && (
+        <div className="space-y-6">
+          <Table>
+            <TableBody>
+              <TableRow>
+                <TableCell className="text-muted-foreground">
+                  Status
+                </TableCell>
+                <TableCell className="flex justify-end">
+                  <OrderStatus status={orderDetails.status} />
+                </TableCell>
+              </TableRow>
 
-            <TableRow>
-              <TableCell className="text-muted-foreground">
-                Cliente
-              </TableCell>
-              <TableCell className="flex justify-end">
-                Ricardo Sobral
-              </TableCell>
-            </TableRow>
+              <TableRow>
+                <TableCell className="text-muted-foreground">
+                  Cliente
+                </TableCell>
+                <TableCell className="flex justify-end">
+                  {orderDetails.customer.name}
+                </TableCell>
+              </TableRow>
 
-            <TableRow>
-              <TableCell className="text-muted-foreground">
-                Telefone
-              </TableCell>
-              <TableCell className="flex justify-end">
-                (11) 99999-9999
-              </TableCell>
-            </TableRow>
+              <TableRow>
+                <TableCell className="text-muted-foreground">
+                  Telefone
+                </TableCell>
+                <TableCell className="flex justify-end">
+                  {orderDetails.customer.phone ?? 'Não informado'}
+                </TableCell>
+              </TableRow>
 
-            <TableRow>
-              <TableCell className="text-muted-foreground">
-                E-mail
-              </TableCell>
-              <TableCell className="flex justify-end">
-                riki.sobral@gmail.com
-              </TableCell>
-            </TableRow>
+              <TableRow>
+                <TableCell className="text-muted-foreground">
+                  E-mail
+                </TableCell>
+                <TableCell className="flex justify-end">
+                  {orderDetails.customer.email}
+                </TableCell>
+              </TableRow>
 
-            <TableRow>
-              <TableCell className="text-muted-foreground">
-                Realizado há
-              </TableCell>
-              <TableCell className="flex justify-end">
-                Há 3 minutos
-              </TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
+              <TableRow>
+                <TableCell className="text-muted-foreground">
+                  Realizado há
+                </TableCell>
+                <TableCell className="flex justify-end">
+                  {formatDistanceToNow(orderDetails.createdAt, {
+                    locale: ptBR,
+                    addSuffix: true
+                  })}
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
 
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead>Produto</TableHead>
-              <TableHead className="text-right">Qtd.</TableHead>
-              <TableHead className="text-right">Preço</TableHead>
-              <TableHead className="text-right">Subtotal</TableHead>
-            </TableRow>
-          </TableHeader>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Produto</TableHead>
+                <TableHead className="text-right">Qtd.</TableHead>
+                <TableHead className="text-right">Preço</TableHead>
+                <TableHead className="text-right">Subtotal</TableHead>
+              </TableRow>
+            </TableHeader>
 
-          <TableBody>
-            <TableRow>
-              <TableCell>Pizza Caipira família</TableCell>
-              <TableCell className="text-right">2</TableCell>
-              <TableCell className="text-right">R$ 59.99</TableCell>
-              <TableCell className="text-right">R$ 119.99</TableCell>
-            </TableRow>
-          </TableBody>
+            <TableBody>
+              {orderDetails.orderItems.map((item) => (
+                <TableRow key={item.id}>
+                  <TableCell>{item.product.name}</TableCell>
+                  <TableCell className="text-right">{item.quantity}</TableCell>
+                  <TableCell className="text-right">{(item.priceInCents / 100).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</TableCell>
+                  <TableCell className="text-right">{(item.priceInCents * item.quantity / 100).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</TableCell>
+                </TableRow>
+              ))}
 
-          <TableFooter>
-            <TableRow>
-              <TableCell colSpan={3}>Total do pedido</TableCell>
-              <TableCell className="text-right font-medium">119.99</TableCell>
-            </TableRow>
-          </TableFooter>
-        </Table>
-      </div>
+            </TableBody>
+
+            <TableFooter>
+              <TableRow>
+                <TableCell colSpan={3}>Total do pedido</TableCell>
+                <TableCell className="text-right font-medium">
+                  {(orderDetails.totalInCents / 100).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}
+                </TableCell>
+              </TableRow>
+            </TableFooter>
+          </Table>
+        </div>
+      )}
+
     </DialogContent>
   )
 }
--- src/pages/app/orders/order-table-row.tsx
+++ src/pages/app/orders/order-table-row.tsx
@@ -6,6 +6,14 @@ import { OrderDetails } from "./order-details";
 import { OrderStatus } from "@/components/order-status";
 import { ptBR } from 'date-fns/locale'
 import { formatDistanceToNow } from 'date-fns'
+import { useState } from "react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { cancelOrder } from "@/api/cancel-order";
+import { queryClient } from "@/lib/react-query";
+import { GetOrdersResponse } from "@/api/get-orders";
+import { approveOrder } from "@/api/approve-order";
+import { deliverOrder } from "@/api/deliver-order";
+import { dispatchOrder } from "@/api/dispatch-order";
 
 export interface OrderTableRowProps {
   order: {
@@ -18,17 +26,75 @@ export interface OrderTableRowProps {
 }
 
 export function OrderTableRow({ order }: OrderTableRowProps) {
+  // nesse momento nos deparamos com um problema no dialog que ele está chamando diversas requisições, pra resolver isso vamos fazer um estado e passar para os dialog, antes dessa mudança ele era uncontrolled agora com esse estado ele é controlled, ai vamos precisar passar para o componente se o details dele está aberto, isso vai para a requisição, onde irá habilitar a requisição
+  const [isDetailsOpen, setIsDetailsOpen] = useState(false);
+  const queryClient = useQueryClient()
+
+  function updateOrderStatusOnCache(orderId: string, status: OrderStatus) {
+    // aqui ele vai pegar todas  as queries datas que possui a querykey orders
+    const cached = queryClient.getQueriesData<GetOrdersResponse>({
+      queryKey: ['orders']
+    })
+
+    //  a baixo fazemos um foreach onde a primeira parte do array é a key a segunda é os dados, sabendo disso vamos ao query client e setaremos o novo dado, pegamos a key que vem ai desestruturamos o que não vamos mudar, vamos apenas no order, ai fazemos um map no order, encontramos por meio do id e ai sim trocamos o status, se os id's não baterem retorna normal
+    cached.forEach(([cacheKey, cacheData],) => {
+      if (!cacheData) {
+        return
+      }
+      queryClient.setQueryData<GetOrdersResponse>(cacheKey, {
+        ...cacheData,
+        orders: cacheData.orders.map((order) => {
+          if (order.orderId === orderId) {
+            return {
+              ...order,
+              status
+            }
+          }
+          return order
+        })
+      })
+    })
+  }
+
+  const { mutateAsync: approveOrderFn, isPending: isApprovingOrder } = useMutation({
+    mutationFn: approveOrder,
+    async onSuccess(_, { orderId }) {
+      updateOrderStatusOnCache(orderId, 'processing')
+    },
+  })
+
+  const { mutateAsync: cancelOrderFn, isPending: isCancelingOrder } = useMutation({
+    mutationFn: cancelOrder,
+    async onSuccess(_, { orderId }) {
+      updateOrderStatusOnCache(orderId, 'canceled')
+    },
+  })
+
+  const { mutateAsync: dispatchOrderFn, isPending: isDispatchingOrder } = useMutation({
+    mutationFn: dispatchOrder,
+    async onSuccess(_, { orderId }) {
+      updateOrderStatusOnCache(orderId, 'delivering')
+    },
+  })
+
+  const { mutateAsync: deliverOrderFn, isPending: isDeliveringOrder } = useMutation({
+    mutationFn: deliverOrder,
+    async onSuccess(_, { orderId }) {
+      updateOrderStatusOnCache(orderId, 'delivered')
+    },
+  })
+
   return (
     <TableRow>
       <TableCell>
-        <Dialog>
+        <Dialog open={isDetailsOpen} onOpenChange={setIsDetailsOpen}>
           <DialogTrigger asChild>
             <Button variant="outline" size="xs">
               <Search className="h-3 w-3" />
               <span className="sr-only">Detalhes do pedido</span>
             </Button>
           </DialogTrigger>
-          <OrderDetails />
+          <OrderDetails orderId={order.orderId} open={isDetailsOpen} />
         </Dialog>
       </TableCell>
       <TableCell className="font-mono text-xs font-medium">
@@ -48,16 +114,50 @@ export function OrderTableRow({ order }: OrderTableRowProps) {
         {order.customerName}
       </TableCell>
       <TableCell className="font-medium">
-        {order.total.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}
+        {(order.total / 100).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}
       </TableCell>
       <TableCell>
-        <Button variant="outline">
-          <ArrowRight className="mr-2 h-3 w-3" />
-          Aprovar
-        </Button>
+        {order.status === 'pending' && (
+          <Button
+            onClick={() => approveOrderFn({ orderId: order.orderId })}
+            variant="outline"
+            disabled={isApprovingOrder}
+          >
+            <ArrowRight className="mr-2 h-3 w-3" />
+            Aprovar
+          </Button>
+        )}
+
+        {order.status === 'processing' && (
+          <Button
+            onClick={() => dispatchOrderFn({ orderId: order.orderId })}
+            variant="outline"
+            disabled={isDispatchingOrder}
+          >
+            <ArrowRight className="mr-2 h-3 w-3" />
+            Em entrega
+          </Button>
+        )}
+
+        {order.status === 'delivering' && (
+          <Button
+            onClick={() => deliverOrderFn({ orderId: order.orderId })}
+            variant="outline"
+            disabled={isDeliveringOrder}
+          >
+            <ArrowRight className="mr-2 h-3 w-3" />
+            Entregue
+          </Button>
+        )}
+
       </TableCell>
       <TableCell>
-        <Button variant="ghost">
+        <Button
+          disabled={!['pending', 'processing'].includes(order.status) || isCancelingOrder}
+          variant="ghost"
+          onClick={() => cancelOrderFn({ orderId: order.orderId })}
+          size="xs"
+        >
           <X className="mr-2 h-3 w-3" />
           Cancelar
         </Button>
